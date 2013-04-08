c2600bb1d293877fbc1f58b3ea9bfecd50840fb3
star82 <tetssuwan@gmail.com>
2013-04-08 13:32:12 +0300

    added  fields_for for adding multiple  condition fields

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < ActiveRecord::Base
-  attr_accessible :title, :description, :discount, :end_at, :start_at, :files_ids
+  attr_accessible :title, :description, :discount, :end_at, :start_at, :files_ids, :conditions_attributes
   attr_accessor :files_ids
   
   has_many :conditions
@@ -7,7 +7,9 @@ class Deal < ActiveRecord::Base
   has_many :addresses
   has_many :tikets
   has_many :images
-  
+
+  accepts_nested_attributes_for :conditions, :allow_destroy => true
+
   after_save :append_attachments
   
   validates_presence_of :description, :discount, :end_at, :start_at, :title
